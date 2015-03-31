9561f5871e20eb7c70627d8d8d718c0c98a41978
Anton Gushcha <ncrashed@gmail.com>
2015-03-31 05:02:19 -0700

    Move distribution function to util

--- src/Util.hs
+++ src/Util.hs
@@ -33,3 +33,24 @@ randomHyperExponential pairs = liftIO $ do
     rng <- newStdGen
     mean <- evalRandT (fromList $ map (second toRational) pairs) rng
     return mean
+
+generationDistr :: String -> [Double] -> Parameter Double
+generationDistr "exponential" [meanTime] = randomExponential meanTime
+generationDistr "exponential" _ = error "Нужен 1 параметр экспоненциальному распределению"
+generationDistr "erlang" [theta, k] = randomErlang theta (floor k)
+generationDistr "erlang" _ = error "Нужно 2 параметра эрланговскому распределению: theta (плавающая запятая) и k (целочисленное)"
+generationDistr "normal" [mu, sigma] = randomNormal mu sigma
+generationDistr "normal" _ = error "Нужно 2 параметра нормальному распределению: mu (математическое ожидание) и sigma (дисперсия)"
+generationDistr "uniform" [minVal, maxVal] = randomUniform minVal maxVal
+generationDistr "uniform" _ = error "Нужно 2 параметра равномерному распределению: minVal (минимальное значение) и maxVal (максимальное значение)"
+generationDistr "hyperexponential" params 
+    | length params `mod` 2 /= 0 = error "Необходимо четное количество параметров гиперэскпоненциального распределения: пара состоит из среднего времени и вероятности."
+    | otherwise = if abs (sumSeconds - 1.0) < 0.001
+        then randomHyperExponential pairs
+        else error "Сумма вероятностей вариантов в гиперэкспоненциальном распределении не равна 1.0"
+        where 
+            pairs = makePairs params
+            sumSeconds = sum $ map snd pairs
+            makePairs (a:b:xs) = (a,b): makePairs xs
+            makePairs _ = []
+generationDistr _ _ = error "Неизвестный закон распределения"
