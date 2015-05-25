cb6dd898ce81c57a927ecf881a86b17763b0151e
Anton Gushcha <ncrashed@gmail.com>
2015-05-25 11:22:37 -0700

    Парсер из файла

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,14 @@
 module Main where
 
-import Task
 import Model
-import Util
 import Tabular
 import Varying
-
--- | Входные параметры модели
-baseInput :: Input
-baseInput = Input {
-    -- | Закон распределения потока заявок
-    -- Указывается один из: exponential, erlang, normal, uniform
-    -- каждому закону нужно указать список параметров
-    -- для exponential нужен 1 параметр (среднее время между заявками)
-    -- для других (erlang и т.д.) нужно по 2 параметра
-    generationDistribution = generationDistr "erlang" [10, 2],
-    inputSystems = [],
-    -- | Время иммитационного моделирования
-    simulationTime = 500000.0,
-    -- | Количество знаков после запятой в выводе результатов
-    outputPrecision = 5
-}
+import Parser
 
 main :: IO ()
-main = putStrLn.prettyPrintOutputs =<< simulateManyBuffers baseInput
-    [ (erlang2, [System erlang2 3, System erlang2 3, System erlang2 3])
-    ] simulate  
-    where
-      erlang2 = generationDistr "erlang" [10, 2]
+main = do
+  minputs <- parseFile "input.txt"
+  case minputs of
+    Left err -> print err
+    Right inputs ->
+      putStrLn.prettyPrintOutputs =<< simulateMany inputs simulate
---
+++ src/Parser.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE RecordWildCards #-}
+module Parser where
+
+import Text.Parsec
+import Text.Parsec.Token
+import Text.Parsec.Language
+import Task
+import Util
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (void)
+
+type Parser a = Parsec String () a
+
+parseFile :: FilePath -> IO (Either ParseError [Input])
+parseFile fname = do 
+  input <- readFile fname
+  return $ runParser (inputParser haskell) () fname input
+  
+inputParser :: TokenParser () -> Parser [Input]
+inputParser TokenParser{..} = do
+  whiteSpace
+  many $ lexeme parseExperiment
+  where
+    parseExperiment = do 
+      void $ symbol "experiment" 
+      between (symbol "{") (symbol "}") $ Input
+        <$> (symbol "input" >> parseDisrt)
+        <*> many (try parseSystem)
+        <*> parseSimulationTime
+        <*> (fromInteger <$> parsePrecision)
+
+    parseSimulationTime = symbol "simulation" >> symbol "time" >> float'
+    parsePrecision = symbol "output" >> symbol "precision" >> natural
+
+    parseSystem :: Parser System
+    parseSystem = do
+      void $ symbol "subsystem"
+      between (symbol "{") (symbol "}") $ System
+        <$> (symbol "processing" >> parseDisrt)
+        <*> (symbol "buffer" >> (fromInteger <$> natural))
+      
+    parseDisrt = choice [
+        erlangParser
+      , expParser
+      , normalParser
+      , uniformParser
+      , hyperExpParser]
+    
+    erlangParser = do
+      void $ symbol "erlang"
+      t <- float'
+      k <- natural
+      return $ generationDistr "erlang" [t, fromIntegral k]
+
+    expParser = do
+      void $ symbol "exponent"
+      t <- float'
+      return $ generationDistr "exponential" [t]
+
+    normalParser = do
+      void $ symbol "normal"
+      mu <- float'
+      sigma <- float'
+      return $ generationDistr "normal" [mu, sigma]
+
+    uniformParser = do
+      void $ symbol "uniform"
+      minv <- float'
+      maxv <- float'
+      return $ generationDistr "uniform" [minv, maxv]
+
+    hyperExpParser = do
+      void $ symbol "hyperexponent"
+      pairs <- many $ between (symbol "(") (symbol ")") $ do
+        t <- float'
+        void $ symbol ","
+        p <- float'
+        return (t, p)
+      return $ generationDistr "hyperexponential" $ concat $ (\(a,b)->[a,b]) <$> pairs
+
+    float' = do
+      mv <- naturalOrFloat
+      return $ case mv of
+        Left i -> fromIntegral i
+        Right v -> v
+      
